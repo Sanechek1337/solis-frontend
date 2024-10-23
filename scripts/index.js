91e249a1a1fb1c14855f121c36e74531ea551530
Alexandr <rednaxela659@gmail.com>
2024-10-23 22:46:43 +0500

    feat: add swiper for all sections with slider

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,7 @@ tabs.forEach((tab) => {
   });
 });
 
-var swiper = new Swiper(".casesSwiper", {
+const casesSwiper = new Swiper(".casesSwiper", {
   slidesPerView: 1,
   spaceBetween: 30,
   pagination: {
@@ -26,6 +26,129 @@ var swiper = new Swiper(".casesSwiper", {
     },
     1024: {
       slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+});
+
+const sevricesSwiper = new Swiper(".sevricesSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+});
+
+const teammatesSwiper = new Swiper(".teammatesSwiper", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    500: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+    1440: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+});
+
+const newsSwiper = new Swiper(".newsSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+});
+
+const feedbackSwiper = new Swiper(".feedbackSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+});
+
+const partnersSwiper = new Swiper(".partnersSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1440: {
+      slidesPerView: 6,
       spaceBetween: 50,
     },
   },
